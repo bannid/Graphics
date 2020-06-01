9f0338a3e0c4ac7b9ab9932fff52a2581cd4f40d
bannidhaliwal <navjotsingh.dhaliwal@connect.qut.edu.au>
2020-06-01 19:24:47 +1000

    Draw the monsters

--- Practice/RayCaster.h
+++ Practice/RayCaster.h
@@ -3,7 +3,15 @@
 #include "BEngine.h"
 
 #define DEBUG_DRAW 1
-
+#define TAU 6.28318530718
+#define M_PI 3.1415926535
+struct Monster {
+	float x;
+	float y;
+	Texture * texture;
+	unsigned int indexInTexture;
+	Monster(float x, float y, Texture * tex, unsigned int index) :x(x), y(y), texture(tex), indexInTexture(index) {}
+};
 class RayCaster : public BEngine {
 	//Dimensions of our world
 	int mapWidth = 16;
@@ -12,7 +20,10 @@ class RayCaster : public BEngine {
 	int windowHeight = 0;
 
 	int screenOffsetX = 0;
-
+	//Monsters
+	std::vector<Monster> monsters;
+	bool monsterVisible = false;
+	std::string toDraw;
 	//Calculated in the OnCreate function
 	int rectangleHeight;
 	int rectangleWidth;
@@ -34,23 +45,24 @@ class RayCaster : public BEngine {
 		BColors::BLUE
 	};
 	Texture walls;
+	Texture monstersTex;
 	//Map of our world -> 16x16
-	std::string map = "1111111131111111"\
-		"1   4          1"\
-		"1   4          1"\
-		"1      4       1"\
-		"1      4       1"\
-		"1      4       1"\
-		"1              1"\
-		"1              1"\
-		"1              1"\
-		"1              1"\
-		"1   444        1"\
-		"1   4 4444444  1"\
-		"1   4          1"\
-		"1   444444444441"\
-		"1              1"\
-		"1111111111111111";
+	std::string map =   "1111111131111111"\
+						"1   4          1"\
+						"1   4          1"\
+						"1      4       1"\
+						"1      4       1"\
+						"1      4       1"\
+						"1              1"\
+						"1              1"\
+						"1              1"\
+						"1              1"\
+						"1   444        1"\
+						"1   4 4444444  1"\
+						"1   4          1"\
+						"1   444444444441"\
+						"1              1"\
+						"1111111111111111";
 	void DrawMap() {
 		for (int i = 0; i < mapHeight; i++) {
 			for (int j = 0; j < mapWidth; j++) {
@@ -137,11 +149,52 @@ class RayCaster : public BEngine {
 			int remappedX = playerX * rectangleWidth;//Remapped X and Y to our world which is 16x16
 			int remappedY = playerY * rectangleHeight;
 			FillRectangle(remappedX, remappedY, remappedX + playerSize, remappedY + playerSize, BColors::WHITE);
+
 			DrawLine(remappedX, remappedY, remappedX + (c * gazeDirectionX* rectangleWidth), remappedY + (c *gazeDirectionY* rectangleHeight), BColors::WHITE);
 			if (std::abs(a - playerAngle) < 0.01) {
 				DrawLine(remappedX, remappedY, remappedX + (c * gazeDirectionX* rectangleWidth), remappedY + (c *gazeDirectionY* rectangleHeight), BColors::RED);
 			}
 #endif
+		}
+		for (auto it = monsters.begin(); it != monsters.end(); it++) {
+			float vectorXOfMonster = it->x - playerX;
+			float vectorYOfMonster = it->y - playerY;
+			float monsterDistance = std::sqrt(vectorXOfMonster * vectorXOfMonster + vectorYOfMonster * vectorYOfMonster);
+			if (monsterDistance < 1.0f)continue;
+			float spriteAngle = std::atan2(vectorYOfMonster, vectorXOfMonster);
+			float spriteAngleBwPlayerGazeDirecton = spriteAngle - playerAngle;
+			if (spriteAngleBwPlayerGazeDirecton > M_PI) spriteAngleBwPlayerGazeDirecton -= 2 * M_PI;
+			if (spriteAngleBwPlayerGazeDirecton < -M_PI) spriteAngleBwPlayerGazeDirecton += 2 * M_PI;
+			int spriteSize = (windowHeight) / monsterDistance;
+			if (spriteSize > 1000) {
+				spriteSize = 1000;
+			}
+			//HOffset is calculated by determining how much percentagle of fov/2 is taken
+			//by the angle bw monster and the player. and then we multiply it by windowWidth/2
+			//because half of fov (fov/2) takes half of the screen.
+			int hOffset = (spriteAngleBwPlayerGazeDirecton / (fov / 2))*(windowWidth / 2);
+			int vOffset = spriteSize / 2;
+			int startingX = windowWidth / 2 + hOffset - spriteSize / 2;
+			int endingX = windowWidth / 2 + hOffset + spriteSize;
+			int startingY = windowHeight / 2 - vOffset;
+			int endingY = windowHeight / 2 + spriteSize;
+			for (int x = startingX; x < endingX; x++) {
+				if (x < 0 || x > windowWidth)continue;
+				for (int y = startingY; y < endingY; y++) {
+					if (y < 0 || y > windowHeight)continue;
+					float normalizedX = (float)(x - startingX) / (endingX - startingX);
+					float normalizedY = (float)(y - startingY) / (endingY - startingY);
+					float percentageOfOneTexture = (float)64 / monstersTex.width;
+					normalizedX = normalizedX / 4.0f + percentageOfOneTexture * it->indexInTexture;
+					BColors::color_t color = GetColorFromTexture(normalizedX, normalizedY, &monstersTex);
+					SetBlendingMode(ALPHA);
+					SetPixel(x, y, color);
+					SetBlendingMode(NORMAL);
+				}
+			}
+			int remappedXForMonster = it->x * rectangleWidth;
+			int remppaedYForMonster = it->y * rectangleHeight;
+			FillRectangle(remappedXForMonster, remppaedYForMonster, remappedXForMonster + playerSize, remppaedYForMonster + playerSize, BColors::RED);
 		}
 
 	}
@@ -181,13 +234,25 @@ class RayCaster : public BEngine {
 		else if (mouseInfo.x > rightOffset) {
 			playerAngle += 1 * elapsedTime * ((float)(mouseInfo.x - leftOffset) / rightOffset);
 		}
+		if (playerAngle > TAU || playerAngle < -TAU) {
+			playerAngle = 0.0f;
+		}
+	}
+	void PopulateNonPlayerCharacters() {
+		monsters.push_back(Monster(10.0f, 10.0f, &monstersTex, 0));
+		monsters.push_back(Monster(8.0f, 10.0f, &monstersTex, 2));
+		monsters.push_back(Monster(6.0f, 10.0f, &monstersTex, 1));
+		monsters.push_back(Monster(4.0f, 10.0f, &monstersTex, 3));
+
 	}
 	virtual bool OnCreate() override {
 		windowHeight = GetScreenHeight();
 		windowWidth = GetScreenWidth();
 		rectangleHeight = 10;
 		rectangleWidth = 10;
-		return LoadTexturePNG("C:\\Users\\Winny-Banni\\Pictures\\walltext.png", &walls, true);
+		PopulateNonPlayerCharacters();
+		return LoadTexturePNG("C:\\Users\\Winny-Banni\\Pictures\\walltext.png", &walls, true) &&
+			LoadTexturePNG("C:\\Users\\Winny-Banni\\Pictures\\monsters.png", &monstersTex, true);
 	}
 
 	virtual bool OnUpdate(float elapsedTime) override {
@@ -198,6 +263,7 @@ class RayCaster : public BEngine {
 		DrawMap();
 #endif
 		HandleInput(elapsedTime);
+		DrawString(toDraw, 0, 0, 50, BColors::WHITE);
 		return true;
 	}
 };
